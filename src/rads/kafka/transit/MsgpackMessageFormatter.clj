2ae6048c81c73253f6a7ce251bc2644dff0b9bf9
Radford Smith <radsmith@gmail.com>
2017-05-27 20:58:19 -0700

    Make the MessageFormatter work with keys

--- src/rads/kafka/transit/MsgpackMessageFormatter.clj
+++ src/rads/kafka/transit/MsgpackMessageFormatter.clj
@@ -9,13 +9,24 @@
     (org.apache.kafka.clients.consumer ConsumerRecord))
   (:gen-class
     :implements [kafka.common.MessageFormatter]
+    :init init-state
+    :state state
     :constructors {[] []}))
 
-(defn -init [this ^Properties props])
+(defn -init [this ^Properties props]
+  (swap! (.state this) assoc :props props))
+
+(defn -init-state []
+  [[] (atom {:props nil})])
+
+(defn read-transit [value]
+  (let [input-stream (ByteArrayInputStream. value)
+        reader (transit/reader input-stream :msgpack)]
+    (transit/read reader)))
 
 (defn -writeTo [this ^ConsumerRecord record ^PrintStream output]
-  (let [input-stream (ByteArrayInputStream. (.value record))
-        reader (transit/reader input-stream :msgpack)]
-    (.println output (pr-str (transit/read reader)))))
+  (when (.get (:props @(.state this)) "print.key")
+    (.print output (str (pr-str (read-transit (.key record))) " ")))
+  (.println output (pr-str (read-transit (.value record)))))
 
 (defn -close [this])
